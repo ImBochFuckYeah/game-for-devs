252826f4227c0051d9de2c1137704223d4bacc8e
Josué Boch <imbochfckyeah@gmail.com>
2025-10-23 12:38:16 -0600

    Initial commit

---
+++ src/main/java/com/umg/game_for_devs/GameForDevsApplication.java
@@ -0,0 +1,13 @@
+package com.umg.game_for_devs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameForDevsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameForDevsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umg/game_for_devs/GameForDevsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umg.game_for_devs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameForDevsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
